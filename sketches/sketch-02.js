bd64041284ebcc4e0ffefc34ba434a8be4951ec3
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-28 17:44:37 +0500

    clock

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,6 +4,10 @@ const settings = {
   dimensions: [1080, 1080]
 };
 
+const degToRad =(deg)=>{
+  return deg / 180 * Math.PI; 
+}
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,18 +15,32 @@ const sketch = () => {
 
     context.fillStyle = 'black'
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+    const w = width * 0.01;
+    const h = height * 0.1;
 
-    context.save();
-    context.translate(x, y)
-    context.rotate(0.3)
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    context.restore();
+    let x, y;
+
+    const num = 12;
+    const radius = width * 0.3;
+
+    for (let i = 0; i < num; i++) {
+      const slice = degToRad(360 / num);
+      const angle = slice * i
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y)
+      context.rotate(-angle)
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+    }
+
   };
 };
 
