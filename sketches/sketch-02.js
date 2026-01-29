8d94c885196a34a18a1de46b2d5a38aad3b0d3bb
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-29 21:22:44 +0500

    random range

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,12 +1,15 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
 
 const settings = {
   dimensions: [1080, 1080]
 };
 
-const degToRad =(deg)=>{
-  return deg / 180 * Math.PI; 
-}
+// const degToRad =(deg)=>{
+//   return deg / 180 * Math.PI; 
+// }
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -26,7 +29,7 @@ const sketch = () => {
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i
 
       x = cx + radius * Math.sin(angle);
@@ -35,6 +38,7 @@ const sketch = () => {
       context.save();
       context.translate(x, y)
       context.rotate(-angle)
+      context.scale(random.range(1, 3), 1)
       context.beginPath();
       context.rect(-w * 0.5, -h * 0.5, w, h);
       context.fill();
